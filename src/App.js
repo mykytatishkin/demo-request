10932159377a89057e5e0f5c042c16f0471f7ccb
Mykyta Tishkin <87891550+GTeasera@users.noreply.github.com>
2023-11-21 17:53:11 +0200

    books loaded

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Client from './Components/Client';
+
+const url = "http://localhost:5221/api/books";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Client url={url}/>
     </div>
   );
 }
--- src/Components/Client.js
+++ src/Components/Client.js
@@ -8,13 +8,40 @@ class Client extends Component {
     }
 
     componentDidMount() {
-        this.getData();
+        this.getDataByFetch();
+    }
+
+    getDataByFetch = () => {
+        fetch(this.props.url)
+        .then( (response) => response.json() )
+        .then( (data) => { this.setState( {items: [...data]}, () => {console.log(this.state);} ) } )
+        .catch( (err) => { console.log(err); });
+    }
+
+    getDataByAxios = () => {
+
     }
 
     render() {
         return (
             <div>
-                Client
+                <h3>Books</h3>
+                <hr/>
+                { this.state.items && 
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Title</th>
+                            <th>Author</th>
+                            <th>Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.items.map( (b, index) => { return <tr key={index}><td>{b.id}</td><td>{b.title}</td><td>{b.author}</td><td>{b.price}</td></tr> })}
+                    </tbody>
+                </table>
+                }
             </div>
         );
     }
